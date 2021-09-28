67b3531c859e38ea501599fa8684993d6d16ea69
Mahesh Rathi <rmaheshrathi115@gmail.com>
2021-09-28 23:57:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/brainmaster/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.brainmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.startBtn_ButtonView);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
